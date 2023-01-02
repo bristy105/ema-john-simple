6588b8478fc86a41b1539c34a681fefa77275e17
brisy105 <bristy105mekuet@gmail.com>
2023-01-02 17:35:20 +0200

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Products</h1>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/shop/Shop.js
@@ -0,0 +1,35 @@
+import React,{useState,useEffect} from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+
+    const [products,setProducts]= useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json()
+        .then(data=>setProducts(data)))
+    },[])
+    return (
+        <div className='shop-container'>
+            <div className="products-cont">
+                {products.map(product=><Product 
+                key={product.id}>
+                    product ={product}
+
+                </Product>)
+
+                }
+                </div>
+                
+            
+            <div className="cart-container">
+                <h1>Order summery</h1>
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Shop;
