02b3348ddb347da32d9f4df70e885e44183aca1b
brisy105 <bristy105mekuet@gmail.com>
2023-01-04 13:59:13 +0200

    setting styles

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,6 +6,16 @@ const Product = ({product}) => {
     return (
         <div className='product'>
             <img src={img} alt=""></img>
+            <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p><small>Price:{price}</small></p>
+            <p><small>Ratings: {ratings} stars</small></p>
+           
+            </div>
+            <button className='btn-cart'>Add to cart</button>
+
+           
+
         </div>
     );
 };
