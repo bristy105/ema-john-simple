03d74fb3d06df39c2a6858d146f15f84cb331894
brisy105 <bristy105mekuet@gmail.com>
2023-01-08 16:33:56 +0200

    quantity added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,43 @@
+
 import React from 'react';
 
 const Cart = (props) => {
     const {cart}= props
+    console.log(cart)
     const handleClear =()=>{
         console.log('clear clicked')
-        
+        }
+        let total = 0;
+        let shipping =0
+        let quantity=0
+for(const product of cart){
+ 
+    
+    total += product.price*product.quantity;
+    shipping +=product.shipping*product.quantity;
+    quantity += product.quantity;
 
-    }
+   
+
+}
+const tax= total*0.1
+const  grandTotal= parseFloat(tax.toFixed(2))+total+shipping
+
+    
+
     return (
         <div>
             <h1>Order Summary </h1>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total price: </p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total price:{total} </p>
+            <p>Total Shipping: {shipping} </p>
+            <p>Tax: {tax}</p>
+            <p>Grand Total : {grandTotal}</p>
 
             <div className='btn'></div>
-            <button onClick={handleClear}>Clear cart</button>
+            <button onClick={handleClear}>
+                <p>Clear cart</p>
+                 </button>
             <button>Review Order</button>
             
             
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -9,19 +10,33 @@ const Shop = () => {
 
     const [cart,setCart]=useState([]);
 
-
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json()
         .then(data=>setProducts(data)))
-    },[])
+    },[]);
+
+    useEffect(()=>{
+        const savedCart = []
+        const storedCart = getStoredCart() ;
+        for (const id in storedCart) {
+            const addedProduct = products.find(product=>product.id===id);
+            if(addedProduct){
+                const quantity =storedCart[id]
+                addedProduct.quantity= quantity
+                console.log(quantity)
+                savedCart.push(addedProduct)
+            }
+            }
+                    
+                setCart(savedCart);
+    },[products])
     const handleClick =(product)=>{
         const newCart= [...cart, product];
        
-        
         setCart(newCart);
-      
-    }
+        addToDb(product.id);
+            }
  
     return (
         <div className='shop-container'>
